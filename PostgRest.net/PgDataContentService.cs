2386c54fae9c1bb2b0f455f81fd8ba806adfdfe0
Vedran Bilopavlović <vbilopav@gmail.com>
2019-08-10 14:25:20 +0200

    Add project files.

---
+++ PostgRest.net/PgDataContentService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Npgsql;
+
+namespace PostgRest.net
+{
+    public interface IPgDataContentService
+    {
+        Task<ContentResult> GetContentAsync(string command, Action<NpgsqlParameterCollection> parameters = null);
+        Task<ContentResult> GetContentAsync(string command, Func<NpgsqlParameterCollection, Task> parameters = null);
+    }
+
+    public class PgDataContentService : IPgDataContentService
+    {
+        private readonly IPgDataService _data;
+        private readonly ILogger<PgDataContentService> _logger;
+        private const string DefaultValue = "{}";
+
+        public PgDataContentService(IPgDataService data, ILogger<PgDataContentService> logger)
+        {
+            _data = data;
+            _logger = logger;
+        }
+
+        public async Task<ContentResult> GetContentAsync(string command, Action<NpgsqlParameterCollection> parameters = null)
+        {
+            try
+            {
+                return new ContentResult
+                {
+                    StatusCode = 200,
+                    Content = await _data.GetString(command, parameters) ?? DefaultValue,
+                    ContentType = "application/json"
+                };
+            }
+            catch (PostgresException e)
+            {
+                _logger.LogError(e, FormatPostgresExceptionMessage(e));
+                return BadRequestContent(e);
+            }
+        }
+
+        public async Task<ContentResult> GetContentAsync(string command, Func<NpgsqlParameterCollection, Task> parameters = null)
+        {
+            try
+            {
+                return new ContentResult
+                {
+                    StatusCode = 200,
+                    Content = await _data.GetString(command, parameters) ?? DefaultValue,
+                    ContentType = "application/json"
+                };
+            }
+            catch (PostgresException e)
+            {
+                _logger.LogError(e, FormatPostgresExceptionMessage(e));
+                return BadRequestContent(e);
+            }
+        }
+
+        private static ContentResult BadRequestContent(PostgresException e) => new ContentResult
+        {
+            StatusCode = 400,
+            Content = JsonConvert.SerializeObject(new
+            {
+                messeage = e.MessageText,
+                details = e.Detail,
+                table = e.TableName,
+                column = e.ColumnName,
+                constraint = e.ConstraintName,
+                error = true
+            }),
+            ContentType = "application/json"
+        };
+
+        private static string FormatPostgresExceptionMessage(PostgresException e) => $"{e.Severity}\n" +
+            $"Message: {e.Message}\n" +
+            $"Detail: {e.Detail}\n" +
+            $"Line: {e.Line}\n" +
+            $"InternalPosition: {e.InternalPosition}\n" +
+            $"Position: {e.Position}\n" +
+            $"SqlState: {e.SqlState}\n" +
+            $"Statement: {e.Statement}\n" +
+            $"ColumnName: {e.ColumnName}\n" +
+            $"ConstraintName: {e.ConstraintName}\n" +
+            $"TableName: {e.TableName}\n" +
+            $"InternalQuery: {e.InternalQuery}\n" +
+            $"Where: {e.Where}\n" +
+            $"Hint: {e.Hint}\n\n";
+    }
+}
---
+++ PostgRest.net/PgDataService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Npgsql;
+
+namespace PostgRest.net
+{
+    public interface IPgDataService
+    {
+        Task<string> GetString(string command, Action<NpgsqlParameterCollection> parameters = null);
+        Task<string> GetString(string command, Func<NpgsqlParameterCollection, Task> parameters = null);
+    }
+
+    public class PgDataService : IPgDataService
+    {
+        private readonly NpgsqlConnection _connection;
+        private readonly ILoggerFactory _loggerFactory;
+        private static readonly IEnumerable<string> InfoLevels = new[] { "INFO", "NOTICE", "LOG" };
+        private static readonly IEnumerable<string> ErrorLevels = new[] { "ERROR", "PANIC" };
+
+        public PgDataService(NpgsqlConnection connection, ILoggerFactory loggerFactory)
+        {
+            _connection = connection;
+            _loggerFactory = loggerFactory;
+        }
+
+        public async Task<string> GetString(string command, Action<NpgsqlParameterCollection> parameters = null)
+        {
+            HandleLogging(command);
+            await _connection.OpenAsync();
+            using (var cmd = new NpgsqlCommand(command, _connection))
+            {
+                parameters?.Invoke(cmd.Parameters);
+                return await GetStringContentFromCommand(cmd);
+            }
+        }
+
+        public async Task<string> GetString(string command, Func<NpgsqlParameterCollection, Task> parameters = null)
+        {
+            HandleLogging(command);
+            await _connection.OpenAsync();
+            using (var cmd = new NpgsqlCommand(command, _connection))
+            {
+                if (parameters != null)
+                {
+                    await parameters?.Invoke(cmd.Parameters);
+                }
+                return await GetStringContentFromCommand(cmd);
+            }
+        }
+
+        private static async Task<string> GetStringContentFromCommand(NpgsqlCommand cmd)
+        {
+            using (var reader = cmd.ExecuteReader())
+            {
+                if (!await reader.ReadAsync())
+                {
+                    return null;
+                }
+                return reader.GetFieldType(0) == DBNull.Value.GetType() ? null : reader.GetString(0);
+            }
+        }
+
+        private void HandleLogging(string command)
+        {
+            var logger = _loggerFactory.CreateLogger(command);
+            _connection.Notice += (sender, args) =>
+            {
+                var severity = args.Notice.Severity;
+                if (InfoLevels.Contains(severity))
+                {
+                    logger.LogInformation(args.Notice.MessageText);
+                }
+                else if (severity == "WARNING")
+                {
+                    logger.LogWarning(args.Notice.MessageText);
+                }
+                else if (severity.StartsWith("DEBUG"))
+                {
+                    logger.LogDebug(args.Notice.MessageText);
+                }
+                else if (ErrorLevels.Contains(severity))
+                {
+                    logger.LogError(args.Notice.MessageText);
+                }
+                else
+                {
+                    logger.LogTrace(args.Notice.MessageText);
+                }
+            };
+        }
+    }
+}
