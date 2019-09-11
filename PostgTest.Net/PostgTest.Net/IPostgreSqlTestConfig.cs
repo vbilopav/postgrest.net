a9d18b04f64cd9d159e771f2a7651d1c4a7caf65
Vedran Bilopavlović <vbilopav@gmail.com>
2019-09-11 14:39:03 +0200

    Move test config to spearate file

---
+++ PostgTest.Net/PostgTest.Net/IPostgreSqlTestConfig.cs
@@ -0,0 +1,24 @@
+﻿namespace PostgTest.Net
+{
+    public interface IPostgreSqlTestConfig
+    {
+        /*
+         * Address of PostgreSql server where tests will be conducted
+         * Default value is `localhost`
+         */
+        string Server { get; set; }
+        int Port { get; set; }
+        string DefaultDatabase { get; set; }
+        string DefaultUser { get; set; }
+        string DefaultUserPassword { get; set; }
+        string TestDatabase { get; set; }
+        string TestUser { get; set; }
+        string TestUserPassword { get; set; }
+        bool CreateTestDatabase { get; set; }
+        string CreateTestDatabaseCommand { get; set; }
+        
+        string CreateTestUserCommand { get; set; }
+        string DropTestDatabaseCommand { get; set; }
+        string DropTestUserCommand { get; set; }
+    }
+}
--- PostgTest.Net/PostgTest.Net/PostgreSqlTestConfig.cs
+++ PostgTest.Net/PostgTest.Net/PostgreSqlTestConfig.cs
@@ -3,24 +3,6 @@ using Npgsql;
 
 namespace PostgTest.Net
 {
-    public interface IPostgreSqlTestConfig
-    {
-        string Server { get; set; }
-        int Port { get; set; }
-        string DefaultDatabase { get; set; }
-        string DefaultUser { get; set; }
-        string DefaultUserPassword { get; set; }
-        string TestDatabase { get; set; }
-        string TestUser { get; set; }
-        string TestUserPassword { get; set; }
-        bool CreateTestDatabase { get; set; }
-        string CreateTestDatabaseCommand { get; set; }
-        
-        string CreateTestUserCommand { get; set; }
-        string DropTestDatabaseCommand { get; set; }
-        string DropTestUserCommand { get; set; }
-    }
-
     public static class PostgreSqlTestConfigExtensions
     {
         public static string GetDefaultConnectionString(this IPostgreSqlTestConfig config) =>
