a07d8000c748bbf80b142ec2d47704fad56e62c5
Vedran Bilopavlović <vbilopav@gmail.com>
2019-09-20 17:57:54 +0200

    Add mixed tests for postgtest

---
+++ Tests/PostgTest.Net.Tests/PostgTest.Net.MixedTests/Config.cs
@@ -0,0 +1,48 @@
+﻿using System.IO;
+using Xunit;
+
+namespace PostgTest.Net.MixedTests
+{
+    public class Config : PostgTestConfig
+    {
+        public override int Port => 5433;
+        public override string TestDatabase => "mixed_tests";
+        public override string TestUser => "mixed_tests_user";
+
+        public override string[] MigrationScripts => new[]
+        {
+            $@"
+            do $$
+            begin
+
+                create table companies (
+                    id int not null generated always as identity primary key,
+                    name varchar(256) not null
+                );
+
+                create table employees (
+                    id int not null generated always as identity primary key,
+                    first_name varchar(256) not null,
+                    last_name varchar(256) not null,
+                    email varchar(1024) not null,
+                    company_id int not null,
+                    constraint fk_employees_company_id__companies_id foreign key (company_id)
+                    references companies (id)
+                    on update no action
+                    on delete cascade
+                    deferrable initially immediate,
+                    unique (email)
+                );
+
+                grant select, insert on companies to {this.TestUser};
+                grant select, insert on employees to {this.TestUser};
+            end
+            $$;"
+        };
+    }
+
+    [CollectionDefinition("PostgreSqlTestDatabase")]
+    public class PostgreSqlFixtureCollection : ICollectionFixture<PostgreSqlDatabaseFixture>
+    {
+    }
+}
---
+++ Tests/PostgTest.Net.Tests/PostgTest.Net.MixedTests/MigrationFixtureTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using PostgExecute.Net;
+using Xunit;
+
+
+namespace PostgTest.Net.MixedTests
+{
+    public class TestMigration : MigrationBase
+    {
+        public override string[] ScriptFiles => new[] { "../../../../Scripts/insert_test_data.sql" };
+    }
+
+    [Collection("PostgreSqlTestDatabase")]
+    public class TestScriptFixtureData : IClassFixture<PostgreSqlTestFixture<TestMigration>>
+    {
+        private readonly PostgreSqlTestFixture<TestMigration> fixture;
+
+        public TestScriptFixtureData(PostgreSqlTestFixture<TestMigration> fixture)
+        {
+            this.fixture = fixture;
+        }
+
+        [Fact]
+        public void TestCompaniesTable()
+        {
+            var read = fixture.TestConnection.Read("select * from companies").ToList();
+            Assert.Single(read);
+            Assert.Equal("vb-software", read.First()["name"]);
+        }
+
+        [Fact]
+        public void TestEmployeesTable()
+        {
+            var read = fixture.TestConnection.Read("select * from employees where company_id = (select id from companies limit 1)").ToList();
+            Assert.Equal(2, read.Count);
+
+            Assert.Equal("Vedran", read[0]["first_name"]);
+            Assert.Equal("Bilopavlović", read[0]["last_name"]);
+            Assert.Equal("vbilopav@gmail.com", read[0]["email"]);
+
+            Assert.Equal("Floki", read[1]["first_name"]);
+            Assert.Equal("The Dog", read[1]["last_name"]);
+            Assert.Equal("vbilopav+floki@gmail.com", read[1]["email"]);
+        }
+    }
+}
